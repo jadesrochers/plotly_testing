e8fa5b60df2d49f2a48e328aaa1c2ab241d84a8b
jadesrochers <jadesrochers@gmail.com>
2024-04-19 20:31:09 -0400

    Functioning basic plot of county data

--- plotly_d3_county_choropleth_script.js
+++ plotly_d3_county_choropleth_script.js
@@ -1,4 +1,5 @@
 const urlGeojsonCounties = 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
+const urlGeojsonStates = ''
 
 async function fetchUrl(url){
     let response = await fetch(url)
@@ -6,31 +7,43 @@ async function fetchUrl(url){
     return data
 }
 
-d3.csv('./time_series_covid19_confirmed_US.csv', plotCsvData)
+d3.csv('./bimonthly_covid19_confirmed_US.csv', plotCsvData)
 
 function unpackData(rows, key) {
     return rows.map(function(row) { return row[key]; });
 }
 
-function extractData(rows) {
+function extractData(rows, geoJson) {
+    // Data includes the geojson, 
+    // and is termed 'choropleth traces' in their API, if you
+    // are trying to find it in a search.
     let data = [{
         type: 'choropleth',
-        locationmode: 'country names',
-        locations: unpackData(rows, 'location'),
-        z: unpackData(rows, 'alcohol'),
-        text: unpackData(rows, 'location'),
-        autocolorscale: true
+        geojson: geoJson,
+        locationmode: 'US Counties',
+        locations: unpackData(rows, 'GEO_ID'),
+        z: unpackData(rows, '4/1/20'),
+        text: unpackData(rows, 'Admin2'),
+        colorscale: 'Jet',
+        colorbar: {
+            title: 'Confirmed Infections'
+        }
     }];
     return data
 }
 
 function setupLayout() {
     let layout = {
-      title: 'Pure alcohol consumption<br>among adults (age 15+) in 2010',
+      title: 'Covid19 Confirmed cases, 4/1/20',
+      // Projection data goes here if you are using it
       geo: {
-          projection: {
-              type: 'robinson'
-          }
+          scope: 'usa',
+          countrycolor: 'rgb(255, 255, 255)',
+          showland: true,
+          landcolor: 'rgb(217, 217, 217)',
+          showlakes: true,
+          lakecolor: 'rgb(255, 255, 255)',
+          subunitcolor: 'rgb(255, 255, 255)'
       }
     };
     return layout
@@ -39,8 +52,8 @@ function setupLayout() {
 // async function plotCsvData(err, rows) {
 async function plotCsvData(err, rows) {
     let countyData = await fetchUrl(urlGeojsonCounties)
-    let data = extractData(rows);
+    let data = extractData(rows, countyData);
     let layout = setupLayout()
-    Plotly.newPlot("plotTarget", data, layout, {showLink: false});
+    Plotly.newPlot("plotTarget", data, layout);
 }
 

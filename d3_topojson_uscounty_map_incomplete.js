3487955be26c25edafa3169eb3ebf86bc20e6b0e
jadesrochers <jadesrochers@gmail.com>
2024-04-26 22:16:50 -0400

    working tooltip, moves with cursor

---
+++ d3_topojson_uscounty_map_incomplete.js
@@ -0,0 +1,185 @@
+//// This was supposed to be a d3 sample of creating a US county map. 
+// The environment it is supposed to run in is the observablehq
+// plotting environment, so I need to work out how to port this 
+// to a plain html/js/css setup and see what that can do.
+// It was wrapped in a chart = {} object, but I am near certain that 
+// tells Observable what to plot, so I would likely instead select
+// an element in my html and add that instead.
+
+
+const { select, json, geoPath, geoNaturalEarth1 } = d3;
+const urlGeojsonCounties = 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
+const urlGeojsonStates = './geojson_us_states_5m.json'
+const urlTopojsonStates = './topojson_us_states_5m.json'
+// d3.csv('./bimonthly_covid19_confirmed_US.csv', plotCsvData)
+
+async function fetchUrl(url){
+    let response = await fetch(url)
+    let data = await response.json()
+    return data
+}
+
+let val = 0.05
+let vals = []
+while (val <=1) {
+    vals.push(val.toPrecision(2))
+    val += 0.05
+}
+let turbos = vals.map(d3.interpolateTurbo)
+const color = d3.scaleQuantize([1, 500000], turbos);
+const path = d3.geoPath();
+const format = d => `${d}%`;
+// const valuemap = new Map(data.map(d => [d.id, d.rate]));
+
+// The counties feature collection is all U.S. counties, each with a
+// five-digit FIPS identifier. The statemap lets us lookup the name of 
+// the state that contains a given county; a state’s two-digit identifier
+// corresponds to the first two digits of its counties’ identifiers.
+// const counties = topojson.feature(us, us.objects.counties);
+// const states = topojson.feature(us, us.objects.states);
+// const statemap = new Map(states.features.map(d => [d.id, d]));
+
+// The statemesh is just the internal borders between states, i.e.,
+// everything but the coastlines and country borders. This avoids an
+// additional stroke on the perimeter of the map, which would otherwise
+// mask intricate features such as islands and inlets. (Try removing
+// the last argument to topojson.mesh below to see the effect.)
+// const statemesh = topojson.mesh(us, us.objects.states, (a, b) => a !== b);
+
+// const svg = d3.select("svg.usaCountyMap")
+//     .attr("width", 975)
+//     .attr("height", 610)
+//     .attr("viewBox", [0, 0, 975, 610])
+//     .attr("style", "max-width: 100%; height: auto;");
+
+// svg.append("g")
+//     .attr("transform", "translate(610,20)")
+//     .append(() => Legend(color, {title: "Unemployment rate (%)", width: 260}));
+
+// svg.append("g")
+//     .selectAll("path")
+//     .data(topojson.feature(us, us.objects.counties).features)
+//     .join("path")
+//     .attr("fill", d => color(valuemap.get(d.id)))
+//     .attr("d", path)
+//     .append("title")
+//     .text(d => `${d.properties.name}, ${statemap.get(d.id.slice(0, 2)).properties.name}\n${valuemap.get(d.id)}%`);
+
+// svg.append("path")
+//     .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
+//     .attr("fill", "none")
+//     .attr("stroke", "white")
+//     .attr("stroke-linejoin", "round")
+//     .attr("d", path);
+//
+
+const Tooltip = d3.select("#mapContainer")
+   .append("div")
+    .classed("tooltip", true)
+    .classed("tooltipOff", true)
+
+const mouseover = function(d) {
+    // Make the tooltip visible
+   Tooltip
+    .classed("tooltipOff", false)
+    .classed("tooltipOn", true)
+    // highlight the county
+    d3.select(this)
+    .style("stroke", "black")
+    .style("opacity", 1)
+}
+
+const mousemove = function(e, d, statemap, valuemap) {
+    Tooltip
+    .style("left", (d3.pointer(e, this)[0]+30) + "px")
+    .style("top", (d3.pointer(e, this)[1]) + "px")
+    .html(`${d.properties.NAME}, ${statemap.get(d.properties.STATE).NAME} <br> ${valuemap.get(d.id)} Cases`)
+}
+
+const mouseleave = function(e) {
+    Tooltip
+      // .style("opacity", 0)
+      .classed("tooltipOff", false)
+      .classed("tooltipOn", true)
+    d3.select(this)
+      .style("stroke", "none")
+      .style("opacity", 0.9)
+}
+
+function configureSvg(svgElement) {
+    svgElement
+        .attr("width", 975)
+        .attr("height", 610)
+        .attr("viewBox", [0, 0, 975, 610])
+        .attr("style", "max-width: 100%; height: auto;");
+    return svgElement
+}
+
+function topLevelElement(svgElement) {
+    svgElement.append("g")
+        .attr("transform", "translate(610,20)")
+        .attr("width", 960)
+        .attr("height", 600)
+        // .append(() => Legend(color, {title: "Covid Confirmed Infections", width: 260}));
+    return svgElement
+}
+
+function appendUsStates(svgElement, stateMesh, geoGenerator) {
+    svgElement.append("path")
+        .datum(stateMesh)
+        .attr("fill", "none")
+        .attr("stroke", "white")
+        .attr("stroke-linejoin", "round")
+        .attr("d", geoGenerator);
+    return svgElement
+}
+
+function appendUsCounties(svgElement, countyFeature, statemap, valuemap, geoGenerator) {
+    svgElement.append("g")
+        .selectAll("path")
+        .data(countyFeature.features)
+        .join("path")
+        .on("mouseover", mouseover)
+        .on("mousemove", (e, d) => mousemove(e, d, statemap, valuemap))
+        .on("mouseleave", mouseleave)
+        .attr("fill", d => color(valuemap.get(d.id)))
+        .attr("d", geoGenerator)
+        .style("opacity", 0.9)
+        .append("title")
+        // .text(d => `${d.properties.NAME}, ${statemap.get(d.properties.STATE).NAME}\n${valuemap.get(d.id)} Cases`);
+    return svgElement
+}
+
+async function plotCsvData() {
+    let covidData = await d3.csv('./bimonthly_covid19_confirmed_US.csv')
+    const valuemap = new Map(covidData.map(d => [d.GEO_ID, Number(d['10/1/21'])]));
+    // I creates the us-states topojson using this cli command: 
+    // geo2topo us-states=geojson_us_states_5m.json > topojson_us_states_5m.json
+    // Get it on system with: npm install -g topojson-server
+    let countyGeojson = await fetchUrl(urlGeojsonCounties)
+    let stateGeojson = await fetchUrl(urlGeojsonStates)
+    let stateTopojson = await fetchUrl(urlTopojsonStates)
+
+    let projection = d3.geoAlbersUsa();
+    let geoGenerator = d3.geoPath().projection(projection);
+    // Get individual coordinate conversion to see if things line up
+    let coord = projection(stateGeojson.features[36].geometry.coordinates[0][0][0])
+    // Get the feature path from the feature
+    // let pathy = generator(stateGeojson.features[36])
+
+    // const counties = topojson.feature(countyData, countyData);
+    // const states = topojson.feature(stateData, stateData);
+    const statemap = new Map(stateGeojson.features.map(d => [d.id, d.properties]));
+    const statemesh = topojson.mesh(stateTopojson, stateTopojson.objects.usStates, (a, b) => a !== b);
+    let svg = d3.select("svg.usaCountyMap")
+    svg = configureSvg(svg)
+    // svg = topLevelElement(svg)
+    svg = appendUsStates(svg, statemesh, geoGenerator)
+    svg = appendUsCounties(svg, countyGeojson, statemap, valuemap, geoGenerator)
+    // let data = extractData(rows, countyData, stateData);
+    // let layout = setupLayout()
+    // let blah = await Plotly.newPlot("plotTarget", data, layout);
+    console.log('doneish')
+}
+
+plotCsvData()
